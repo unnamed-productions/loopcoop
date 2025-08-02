c814202c5ce6bc83173ed0c7875a90037b4ec4b2
Simon <32185431+SimonK5@users.noreply.github.com>
2025-08-02 16:53:44 -0400

    compilation fixes; chicken logic; enemy idle (#2)

---
+++ Assets/Scripts/Enemy/EnemyIdleBehaviour.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleBehavior : MonoBehaviour
+{
+    private Vector2 idleWalkDirection;
+
+    float minIdleCooldown = 0f;
+
+    float maxIdleCooldown = 4f;
+
+    private bool isWalking;
+
+    [SerializeField]
+    float idleWalkSpeed = 0.4f;
+
+    [SerializeField]
+    private EnemyBehaviour enemyState;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        idleWalkDirection = Random.insideUnitCircle.normalized;
+        ToggleWalk();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (enemyState.currentState)
+        {
+            case EnemyBehaviour.EnemyState.NEUTRAL:
+                if (isWalking)
+                {
+                    rb.velocity = idleWalkSpeed * idleWalkDirection;
+                }
+                else
+                {
+                    rb.velocity = Vector2.zero;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    void ToggleWalk()
+    {
+        isWalking = true;
+        idleWalkDirection = Random.insideUnitCircle.normalized;
+        float walkTime = Random.Range(minIdleCooldown, maxIdleCooldown);
+
+        Invoke("ToggleStand", walkTime);
+    }
+
+    void ToggleStand()
+    {
+        isWalking = false;
+        float standTime = Random.Range(minIdleCooldown, maxIdleCooldown);
+        Invoke("ToggleWalk", standTime);
+    }
+}
---
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Vector2 direction;
+    Rigidbody2D rb;
+    
+    const float FIREBALL_SPEED = 8f;
+
+    [SerializeField]
+    int damage = 2;
+    [SerializeField]
+    float knockbackForce = 15f;
+
+    [SerializeField]
+    Animator animator;
+
+    [SerializeField]
+    bool shouldFlip;
+
+    [SerializeField]
+    SpriteRenderer sprite;
+
+    [SerializeField]
+    float maxRange = 50f;
+
+    Vector2 startPosition;
+
+    Transform owner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPosition = rb.position;
+    }
+
+    public void Initialize(Vector2 dirn, int damage, Transform owner)
+    {
+        direction = new Vector2(dirn.x, dirn.y);
+        float angle = Mathf.Atan2(dirn.y, dirn.x) * Mathf.Rad2Deg;
+        if(shouldFlip && Mathf.Abs(angle) > 90){
+            sprite.flipY = true;
+        }
+        Debug.Log("Angle:" + angle);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        this.damage = damage;
+        this.owner = owner;
+        // animator.Play("Default");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.instance.currentGameState != GameManager.GameState.PAUSED)
+        {
+            rb.velocity = direction * FIREBALL_SPEED;
+        }
+
+        if ((rb.position - startPosition).magnitude > maxRange)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform == owner) return;
+
+        // if collision is player, call function to deal damage
+        if (other.gameObject.name == "Player")
+        {
+            TopDownPlayer player = other.gameObject.GetComponent<TopDownPlayer>();
+            // player.Hit(damage, 0.2f, direction.normalized * knockbackForce);
+            Destroy(gameObject);
+        }
+
+        // TODO: if it hits yarn, just destroy it
+
+        // Destroy(gameObject);
+        
+    }
+}
