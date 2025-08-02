cbe240174198f8cbd1e2b04c335fcb024b09c7a1
Noah <noahbraun20@gmail.com>
2025-08-02 18:34:24 -0400

    Swinging Ball (#4)
    * No physics draft
    
    * Physics movement
    
    * Parameters and stopping
    
    * First Working Implementation
    
    * All basic functionality
    
    * Clean up print statements
    
    * Fix merge conflict
    
    ---------
    
    Co-authored-by: Caroline Hohner <crh255@cornell.edu>

---
+++ Assets/Noah-Git/Scripts/YarnBall/YarnBallHittable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class YarnBallHittable : MonoBehaviour
+{
+    [SerializeField]
+    CameraShake camShake;
+    [SerializeField]
+    float cameraShakeDur;
+    [SerializeField]
+    float cameraShakeIntensity;
+
+    [SerializeField] public UnityEvent onHit;
+
+    public void hitMe(float intensity)
+    {
+        camShake.ShakeCamera(cameraShakeDur, intensity);
+        onHit.Invoke();
+    }
+
+    public void hitMe()
+    {
+        hitMe(cameraShakeIntensity);
+    }
+
+    public void SampleEvent()
+    {
+        print("My name is " + gameObject.name + "\nOuuughhh");
+    }
+}
