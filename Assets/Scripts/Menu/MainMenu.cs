654eb060887784f0ee708031cf0fddd3c514068e
Elaine <eyr4@cornell.edu>
2025-08-03 02:16:25 -0400

    basic main menu w start to movement

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,8 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        GameManager.instance.StartGame();
+        SceneManager.LoadScene("Movement");
     }
 
     public void OpenSettings()
